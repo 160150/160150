b9664dacb10de2744e8d311a11c88997dc51bcdf
160150 <yse160150@gmail.com>
2016-11-24 11:57:22 +0900

    ４回に１回英語でALOHAにしました。

--- 20161124/aloha.java
+++ 20161124/aloha.java
@@ -1,8 +1,14 @@
 public class aloha{
 	 public static void main(String[] args) {
 	 	for (int i=0;i<20;i++ ) {
+	 		if (i%4==0) {
+	 			System.out.println("aloha");
+	
+	 		}else {
 	 	
 	 System.out.println(i+"アロハ！");
 }
+
 	}
+}
 }
