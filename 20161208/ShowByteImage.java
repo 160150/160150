303d591cc4fcaf946efa24e1511d32c1481040be
160150 <yse160150@gmail.com>
2016-12-08 11:31:55 +0900

    画像ファイルをコピーしました。

---
+++ 20161208/ShowByteImage.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class 	ShowByteImage{
+	public static void main(String[] args) {
+		FileInputStream inFile = null;
+		try{
+
+     inFile = new FileInputStream("Image/n(1).jpg");
+    int data;
+    int n= 0;
+    while((data = inFile.read())!= -1){
+    	n++;
+    
+  	System.out.println("くり返し: "+n+"回:"+data);
+	
+    }
+    	
+
+	
+
+	
+    inFile.close();
+
+		}catch(IOException e){
+			System.err.println("エラーです。");
+			System .exit(1);
+		}
+	}
+
+}
