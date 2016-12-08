b52896922a3f8bbb50bed64f27e1c4b9eee8dc16
160150 <yse160150@gmail.com>
2016-12-08 10:01:21 +0900

    Aloha/Echo.java つくりました。

---
+++ 20161208/Aloha.java
@@ -0,0 +1,13 @@
+public class Aloha{
+	public static void main(String[] args) {
+		for (int i=0;i<30 ;i++ ) {
+			System.out.println(i+"Aloha");
+       } 
+       int i = 0;
+       while (i<30) {
+       	System.out.println("Aloha");
+       	i++;
+        	
+        } 
+     }
+   }
---
+++ 20161208/Echo.java
@@ -0,0 +1,9 @@
+public class Echo{
+	public static void main(String[] args) {
+	String data = new java.util.Scanner(System.in).nextline();
+	System.out.println(data);
+	if (data.equals("Aoki") ) {
+		break;
+	}
+  }
+}
