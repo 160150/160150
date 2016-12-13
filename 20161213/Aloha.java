45db0b57f652329b032a191c59c4c61efe7469f6
160150 <yse160150@gmail.com>
2016-12-13 09:34:45 +0900

    アロハをカタカナで４回に１回　表示しました。

---
+++ 20161213/Aloha.java
@@ -0,0 +1,14 @@
+public class Aloha  {
+	public static void main(String[] args) {
+		for (int i=0;i<20 ;i++) {
+		if (i%4==0) {
+		System.out.println(i+"アロハ");
+
+			}else {
+		System.out.println(i+"Aloha");	
+		}
+
+		}
+	}
+	
+}
