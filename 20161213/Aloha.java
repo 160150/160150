1af09d68f0dc34c69a65e6ba963d8567d28f572f
160150 <yse160150@gmail.com>
2016-12-13 10:06:57 +0900

    exit を　できました。

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,14 +1,14 @@
 public class Aloha  {
 	public static void main(String[] args) {
-		
-			System.out.println("input data");
+	while(true){
+				System.out.println("input data");
 			String data = new java.util.Scanner(System.in ).nextLine();
 			System.out.println(data);
 			if (data.equals ("exit")) {
-				System.out.println("おわりました");
+				break;
 					
 				}	
-
+          }
 		}
 	}
 	
