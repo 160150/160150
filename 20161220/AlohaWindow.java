b7eee973703d080f73ac5b121e0d9677ebf96b8d
160150 <yse160150@gmail.com>
2016-12-20 11:42:06 +0900

    AlohaWindow.java updated.

--- 20161220/AlohaWindow.java
+++ 20161220/AlohaWindow.java
@@ -3,9 +3,18 @@ import javax.swing.*;
 public class AlohaWindow{
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.setSize(200,200);
-		frame.setVisible(true);
+		frame.setSize(400,200);
+		frame.setTitle("カナル");
 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout( new FlowLayout() );
+
+		JLabel label = new JLabel("HELLo");
+			Container ctnr =frame.getContentPane();		
+			ctnr.add(label);
+            JButton button = new JButton("Click Me!");
+			ctnr.add(button);
+		frame.setVisible(true);
 			
 		}
 	}
